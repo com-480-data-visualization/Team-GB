0c5b110365f6f1d59ea57de50ed6fe38e3b85d88
Rodrigue de Guerre <rodrigue.deguerre@epfl.ch>
2025-05-27 20:52:18 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 const index = (app) => {
     "use strict";
 
+    app.get('/board', (req, res, next) => {
+        return res.render('./views/board.html', {});
+    });
     app.get('/pieces', (req, res, next) => {
         return res.render('./views/pieces.html', {});
+    });
+
+    app.get('/credits', (req, res, next) => {
+        return res.render('./views/credits.html', {});
     });
 };
 
