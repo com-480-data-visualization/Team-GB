42d2c9971b92cb7c1c28929a9b7dc13e30caec99
Rodrigue de Guerre <rodrigue.deguerre@epfl.ch>
2025-05-30 17:14:46 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ const index = (app) => {
     app.get('/basics', (req, res, next) => {
         return res.render('./views/chess-basics.html', {});
     });
-    app.get('/pieces', (req, res, next) => {
-        return res.render('./views/pieces.html', {});
-    });
-    app.get('/board', (req, res, next) => {
-        return res.render('./views/board.html', {});
+    // app.get('/pieces', (req, res, next) => {
+    //     return res.render('./views/pieces.html', {});
+    // });
+    // app.get('/board', (req, res, next) => {
+    //     return res.render('./views/board.html', {});
+    // });
+    app.get('/about', (req, res, next) => {
+        return res.render('./views/about.html', {});
     });
 
     app.get('/credits', (req, res, next) => {
